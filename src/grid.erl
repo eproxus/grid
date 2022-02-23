dcc8f85037424b868af53e7d55507be32c0485fc
Adam Lindberg <hello@alind.io>
2022-02-23 10:01:32 +0100

    Support maps and tuple rows, and header/column options

--- src/grid.erl
+++ src/grid.erl
@@ -6,65 +6,141 @@
 -export([format/2]).
 -ignore_xref({format, 2}).
 
+%--- Macros --------------------------------------------------------------------
+
+-define(cell(Item, Length), {'$cell', Length, Item}).
+
 %--- API -----------------------------------------------------------------------
 
 format(Items) -> format(Items, #{}).
 
 format(Items, Opts) ->
-    {Rows, Columns} = process(Items, columns(maps:get(columns, Opts, []))),
+    {Rows, Columns} = process(Items, columns(Opts), opts(Opts)),
     render(Rows, Columns, Opts).
 
 %--- Internal ------------------------------------------------------------------
 
-process(Items, Columns) ->
-    {Rows, AllColumns} = process(Items, Columns, []),
+opts(Opts) -> maps:map(fun opt/2, Opts).
+
+opt(header, true) -> #{format => fun(V) -> V end};
+opt(header, Format) when is_atom(Format) -> #{format => Format};
+opt(columns, []) -> error(empty_columns);
+opt(_K, V) -> V.
+
+columns(Opts) -> columns(maps:get(columns, Opts, []), 1, #{}).
+
+columns([], Index, Acc) ->
+    {Index, Acc};
+columns([Column | Columns], Index, Acc) when is_atom(Column) ->
+    NewAcc = maps:put(Index, #{key => Column, index => Index}, Acc),
+    columns(Columns, Index + 1, NewAcc);
+columns([I | Columns], Index, Acc) when is_integer(I) ->
+    NewAcc = maps:put(I, #{index => Index}, Acc),
+    columns(Columns, I, NewAcc);
+columns([#{index := I} = Column | Columns], _Index, Acc) ->
+    case maps:find(I, Acc) of
+        {ok, Existing} -> error({duplicate_index, Column, Existing});
+        error -> columns(Columns, I + 1, maps:put(I, Column, Acc))
+    end;
+columns([Column | Columns], Index, Acc) when is_map(Column) ->
+    columns(Columns, Index + 1, maps:put(Index, Column#{index => Index}, Acc)).
+
+process(Items, Columns, Opts) ->
+    {Rows, AllColumns} = process(Items, Columns, [], Opts),
     {Rows, filter_columns(Columns, AllColumns)}.
 
-process([], Columns, Items) ->
-    {
-        lists:reverse(Items),
-        lists:sort(
-            fun(#{index := I1}, #{index := I2}) -> I1 =< I2 end,
-            column_index(Columns)
-        )
-    };
-process([Item | Rest], Columns, Items) ->
-    {Formatted, NewColumns} = process_item(Item, Columns),
-    process(Rest, NewColumns, [Formatted | Items]).
+process([], Columns, Rows, Opts) ->
+    {Header, {_Index, NewColumns}} = process_headers(Columns, Opts),
+    AllRows = Header ++ lists:reverse(Rows),
+    IndexCompare = fun(#{index := I1}, #{index := I2}) -> I1 =< I2 end,
+    SortedColumns = lists:sort(IndexCompare, maps:values(NewColumns)),
+    {AllRows, SortedColumns};
+process([Item | Rest], Columns, Rows, Opts) ->
+    {Row, NewColumns} = process_row(Item, Columns),
+    process(Rest, NewColumns, [Row | Rows], Opts).
 
-process_item(Item, Columns) when is_list(Item) ->
-    process_item_list(Item, 1, {[], Columns});
-process_item(Item, Columns) when is_map(Item) ->
-    process_item_map(Item, Columns).
+process_headers({Index, Columns}, #{header := Header}) ->
+    Process = fun(_, Column, Acc) -> process_header(Header, Column, Acc) end,
+    Item = lists:reverse(maps:fold(Process, [], Columns)),
+    {Formatted, NewColumns} = process_row(Item, {Index, Columns}),
+    {[Formatted], NewColumns};
+process_headers(Columns, _Opts) ->
+    {[], Columns}.
 
-process_item_list([], _Pos, {Item, Columns}) ->
-    {lists:reverse(Item), Columns};
-process_item_list([Cell | Rest], Pos, {Item, Columns}) ->
-    {Rendered, Length} = render_cell(Cell),
-    NewColumns = update_columns(index, Pos, #{width => Length}, Columns),
-    process_item_list(Rest, Pos + 1, {[Rendered | Item], NewColumns}).
+process_header(Header, #{name := Name}, Acc) ->
+    [format_header(Header, Name) | Acc];
+process_header(Header, #{key := Key}, Acc) ->
+    [format_header(Header, render_cell_value(Key)) | Acc];
+process_header(Header, #{index := I}, Acc) ->
+    [format_header(Header, render_cell_value(I)) | Acc].
 
-process_item_map(Cells, Columns) ->
-    maps:fold(
-        fun(Key, Value, {Acc, Cs}) ->
-            {Rendered, Length} = render_cell(Value),
-            NewColumns = update_columns(
-                key, Key, #{width => Length, key => Key}, Cs
-            ),
-            {Acc#{Key := Rendered}, NewColumns}
+process_row(Row, Columns) ->
+    process_row(next(iter(Row)), Columns, {}).
+
+process_row(none, Columns, Acc) ->
+    {Acc, Columns};
+process_row({Item, Pos, Iter}, Columns, Acc) ->
+    Cell = process_cell(Item),
+    {Index, NewColumns} =
+        update_columns(Pos, #{width => cell_length(Cell)}, Columns),
+    NewAcc = set_cell(Acc, Index, Cell),
+    process_row(next(Iter), NewColumns, NewAcc).
+
+cell_length(?cell(_Text, Length)) -> Length.
+
+set_cell(Row, Index, Cell) when Index =< tuple_size(Row) ->
+    setelement(Index, Row, Cell);
+set_cell(Row, Index, Cell) when Index == tuple_size(Row) + 1 ->
+    erlang:append_element(Row, Cell);
+set_cell(Row, Index, Cell) ->
+    set_cell(erlang:append_element(Row, '_'), Index, Cell).
+
+iter(Item) when is_list(Item) -> {list, 1, Item};
+iter(Item) when is_tuple(Item) -> {tuple, 1, Item};
+iter(Item) when is_map(Item) -> {map, 1, maps:next(maps:iterator(Item))};
+iter(Item) -> error({unknown_row_type, Item}).
+
+next({list, _Index, []}) ->
+    none;
+next({list, Index, [Elem | Rest]}) ->
+    {Elem, {index, Index}, {list, Index + 1, Rest}};
+next({tuple, Index, Tuple}) when Index > tuple_size(Tuple) ->
+    none;
+next({tuple, Index, Tuple}) ->
+    {element(Index, Tuple), {index, Index}, {tuple, Index + 1, Tuple}};
+next({map, _Index, none}) ->
+    none;
+next({map, Index, {Key, Value, NewIter}}) ->
+    {Value, {key, Key}, {map, Index + 1, NewIter}}.
+
+process_cell(?cell(Term, Length)) ->
+    ?cell(render_cell_value(Term), Length);
+process_cell(Term) ->
+    Rendered = render_cell_value(Term),
+    ?cell(Rendered, string:length(Rendered)).
+
+update_columns({index, Value}, Attrs, {Index, Columns}) ->
+    NewColumns =
+        case maps:find(Value, Columns) of
+            {ok, Column} ->
+                {Index, maps:put(Value, update_column(Column, Attrs), Columns)};
+            error ->
+                {Index + 1, maps:put(Index, Attrs#{index => Index}, Columns)}
         end,
-        {Cells, Columns},
-        Cells
-    ).
+    {Value, NewColumns};
+update_columns({Key, Value}, Attrs, {Index, Columns}) ->
+    First = maps:next(maps:iterator(Columns)),
+    update_columns(Key, Value, Attrs, {Index, Columns}, First).
 
-update_columns(Key, Value, Updates, []) ->
-    [maps:put(Key, Value, Updates)];
-update_columns(Key, Value, Updates, [Column | Columns]) ->
+update_columns(Key, Value, Attrs, {Index, Columns}, none) ->
+    NewColumns = maps:put(Index, Attrs#{Key => Value, index => Index}, Columns),
+    {Index, {Index + 1, NewColumns}};
+update_columns(Key, Value, Attrs, {Index, Columns}, {I, Column, Iter}) ->
     case Column of
-        #{Key := Value} ->
-            [update_column(Column, Updates) | Columns];
-        Other ->
-            [Other | update_columns(Key, Value, Updates, Columns)]
+        #{Key := Value, index := I} ->
+            {I, {Index, maps:put(I, update_column(Column, Attrs), Columns)}};
+        _Other ->
+            update_columns(Key, Value, Attrs, {Index, Columns}, maps:next(Iter))
     end.
 
 update_column(Column, Updates) ->
@@ -73,6 +149,7 @@ update_column(Column, Updates) ->
         (_, _, V) -> V
     end,
     % FIXME: When supporting OTP 24+ only, use maps:merge_with/3 instead
+    % maps:merge_with(Update, Column, Updates).
     maps:fold(
         fun(Key, Value, C) ->
             case maps:find(Key, C) of
@@ -83,46 +160,38 @@ update_column(Column, Updates) ->
         Column,
         Updates
     ).
-% maps:merge_with(Update, Column, Updates).
 
 render(Items, Columns, Opts) ->
     lists:map(fun(Item) -> render_row(Item, Columns, Opts) end, Items).
 
-render_row(Item, Columns, Opts) when is_list(Item) ->
-    render_row_list(Item, Columns, Opts);
-render_row(Item, Columns, Opts) when is_map(Item) ->
-    render_row_map(Item, Columns, Opts).
-
-render_row_list([], [], _Opts) ->
+render_row({}, _Columns, _Opts) ->
     [$\n];
-render_row_list([Cell | Row], [#{width := Width} | Columns], Opts) ->
-    Formatted = io_lib:format("~*.. s", [-Width, Cell]),
-    [
-        [Formatted | [spacer(Opts) || Row =/= []]]
-        | render_row_list(Row, Columns, Opts)
-    ].
+render_row({Cell}, [Column | _], _Opts) ->
+    [render_cell(Cell, Column, no_padding), $\n];
+render_row(Row, [Column], _Opts) ->
+    [render_cell(element(1, Row), Column, no_padding), $\n];
+render_row(Row, [Column | Columns], Opts) ->
+    Rendered = render_cell(element(1, Row), Column, padding),
+    [Rendered, render_cells(Row, 2, Columns, Opts), $\n].
 
-render_row_map(Row, Columns, Opts) when is_map(Row) ->
-    [
-        lists:join(
-            spacer(Opts),
-            lists:foldr(
-                fun(#{key := Key, width := Width}, Acc) ->
-                    Formatted = io_lib:format("~*.. s", [
-                        -Width, maps:get(Key, Row)
-                    ]),
-                    [Formatted | Acc]
-                end,
-                [],
-                Columns
-            )
-        ),
-        $\n
-    ].
+render_cells(_Row, _Index, [], _Opts) ->
+    [];
+render_cells(Row, Index, [Column | _], Opts) when Index == tuple_size(Row) ->
+    [spacer(Opts), render_cell(element(Index, Row), Column, no_padding)];
+render_cells(Row, Index, [Column], Opts) ->
+    [spacer(Opts), render_cell(element(Index, Row), Column, no_padding)];
+render_cells(Row, Index, [Column | Columns], Opts) ->
+    Rendered = render_cell(element(Index, Row), Column, padding),
+    [spacer(Opts), Rendered | render_cells(Row, Index + 1, Columns, Opts)].
 
-render_cell(Term) ->
-    Rendered = render_cell_value(Term),
-    {Rendered, string:length(Rendered)}.
+render_cell('_', _Column, no_padding) ->
+    [];
+render_cell('_', #{width := CW}, padding) ->
+    lists:duplicate(CW, $\s);
+render_cell(?cell(Text, W), #{width := CW}, padding) when W =< CW ->
+    [Text, lists:duplicate(CW - W, $\s)];
+render_cell(?cell(Text, _W), _Column, no_padding) ->
+    [Text].
 
 render_cell_value(Term) when is_binary(Term); is_list(Term) ->
     Term;
@@ -131,32 +200,19 @@ render_cell_value(Term) when is_integer(Term) ->
 render_cell_value(Term) when is_atom(Term) ->
     atom_to_binary(Term, utf8).
 
-columns(Columns) -> columns(Columns, 1, []).
-
-columns([], _Pos, Acc) ->
-    lists:reverse(Acc);
-columns([Column | Columns], Pos, Acc) ->
-    columns(Columns, Pos + 1, [#{key => Column, index => Pos} | Acc]).
-
-column_index([#{index := Index} = Column | Columns]) ->
-    [Column | column_index(Columns, Index)];
-column_index(Columns) ->
-    column_index(Columns, 1).
-
-column_index(Columns, Index) ->
-    {Indexed, _} = lists:mapfoldl(
-        fun
-            (#{index := I} = Column, I) -> {Column, I + 1};
-            (#{index := I2}, I2) -> error({index_mismatch, I2, I2, Columns});
-            (Column, I) -> {Column#{index => I}, I + 1}
-        end,
-        Index,
-        Columns
-    ),
-    Indexed.
-
-filter_columns([], Columns) -> Columns;
-filter_columns(Desired, All) -> lists:sublist(All, length(Desired)).
+filter_columns({_, Desired}, Columns) when map_size(Desired) == 0 -> Columns;
+filter_columns({_, Desired}, All) -> lists:sublist(All, maps:size(Desired)).
 
 spacer(#{spacer := Spacer}) -> Spacer;
 spacer(_Opts) -> <<"  ">>.
+
+format_header(#{format := Format}, Header) ->
+    format_text(Format, Header).
+
+format_text(uppercase, Binary) -> string:uppercase(words(Binary));
+format_text(titlecase, Binary) -> string:titlecase(words(Binary));
+format_text(lowercase, Binary) -> string:lowercase(words(Binary));
+format_text(Fun, Binary) when is_function(Fun, 1) -> Fun(Binary);
+format_text(Format, _Binary) -> error({invalid_format, Format}).
+
+words(Binary) -> string:replace(Binary, <<"_">>, <<" ">>, all).
--- test/grid_tests.erl
+++ test/grid_tests.erl
@@ -15,34 +15,72 @@
 
 empty_test() -> ?equal("", []).
 
-simple_test() ->
+list_test() ->
     ?equal(
         [
-            "1    3.14  foo",
-            "bar  2     baz"
+            "1",
+            "2  foo   c",
+            "3  quux"
         ],
         [
-            [1, "3.14", foo],
-            [bar, 2, <<"baz">>]
+            [1],
+            [2, foo, "c"],
+            [3, <<"quux">>]
+        ]
+    ).
+
+tuple_test() ->
+    ?equal(
+        [
+            "1",
+            "2  foo   c",
+            "3  quux"
+        ],
+        [
+            {1},
+            {2, foo, "c"},
+            {3, <<"quux">>}
         ]
     ).
 
 maps_test() ->
     ?equal(
         [
-            "foobar  1  ",
-            "baz     295"
+            "1",
+            "2  c  bar",
+            "3     bar"
         ],
         [
-            #{type => foobar, value => 1},
-            #{type => baz, value => 295}
+            #{type => 1},
+            #{type => 2, value => bar, extra => "c"},
+            #{type => 3, value => <<"bar">>}
         ]
+    ).
+
+mixed_rows_test() ->
+    ?equal(
+        [
+            "1",
+            "2  foo   c",
+            "3  quux"
+        ],
+        [
+            #{type => 1},
+            [2, foo, "c"],
+            {3, <<"quux">>}
+        ]
+    ).
+
+empty_columns_test() ->
+    ?assertError(
+        empty_columns,
+        grid:format([[a], [b]], #{columns => []})
     ).
 
 maps_column_test() ->
     ?equal(
         [
-            "foobar  1  ",
+            "foobar  1",
             "baz     295"
         ],
         [
@@ -52,17 +90,52 @@ maps_column_test() ->
         #{columns => [type, value]}
     ).
 
-mixed_rows_test() ->
+maps_column_spec_test() ->
     ?equal(
         [
             "1    foobar",
-            "baz  295   "
+            "295  baz"
         ],
         [
             #{type => foobar, value => 1},
-            [baz, 295]
+            #{type => baz, value => 295}
         ],
-        #{columns => [value, type]}
+        #{columns => [#{key => value}, type]}
+    ).
+
+maps_column_disorder_test() ->
+    ?equal(
+        [
+            "2  3  1",
+            "5  6  4"
+        ],
+        [
+            #{a => 1, b => 2, c => 3},
+            #{a => 4, b => 5, c => 6}
+        ],
+        #{
+            columns => [
+                #{key => a, index => 3},
+                #{key => b, index => 1},
+                #{key => c, index => 2}
+            ]
+        }
+    ).
+
+maps_column_same_index_test() ->
+    ?assertError(
+        {duplicate_index, _, _},
+        grid:format(
+            [
+                #{type => foobar, value => 1},
+                #{type => baz, value => 295}
+            ],
+            #{
+                columns => [
+                    #{key => value, index => 1}, #{key => type, index => 1}
+                ]
+            }
+        )
     ).
 
 custom_spacer_test() ->
@@ -77,6 +150,94 @@ custom_spacer_test() ->
         ],
         #{spacer => $|}
     ).
+
+empty_header_test() -> ?equal([], [], #{header => true}).
+
+implicit_header_test() ->
+    ?equal(
+        [
+            "1  2  3",
+            "a",
+            "a  b  c",
+            "a  b"
+        ],
+        [
+            [a],
+            [a, b, c],
+            [a, b]
+        ],
+        #{header => true}
+    ).
+
+column_header_test() ->
+    ?equal(
+        [
+            "value  type",
+            "1      foobar",
+            "baz    295"
+        ],
+        [
+            #{type => foobar, value => 1},
+            [baz, 295]
+        ],
+        #{header => true, columns => [value, type]}
+    ).
+
+column_name_test() ->
+    ?equal(
+        [
+            "VALUE  TYPE",
+            "1      foobar",
+            "baz    295"
+        ],
+        [
+            #{type => foobar, value => 1},
+            [baz, 295]
+        ],
+        #{
+            header => true,
+            columns => [
+                #{key => value, name => "VALUE"},
+                #{key => type, name => "TYPE"}
+            ]
+        }
+    ).
+
+header_format_test() ->
+    ?equal(
+        [
+            "FOO     BAZ",
+            "foobar  quuuux"
+        ],
+        [#{foo => foobar, bar => bar, baz => quuuux}],
+        #{header => uppercase, columns => [foo, baz]}
+    ).
+
+header_format_only_test_() ->
+    Test = fun(Key, Expected, Format) ->
+        fun() ->
+            ?equal(
+                [
+                    Expected,
+                    "x"
+                ],
+                [#{Key => x, other => y}],
+                #{
+                    header => #{format => Format},
+                    columns => [Key]
+                }
+            )
+        end
+    end,
+    {inparallel, [
+        Test(foo_bar, "FOO BAR", uppercase),
+        Test(foo_bar, "Foo bar", titlecase),
+        Test('FOO_BAR', "foo bar", lowercase),
+        Test('FOO_BAR', "RAB_OOF", fun(S) -> string:reverse(S) end),
+        Test('FOO_BAR', "RAB_OOF", fun(S) ->
+            {'$cell', string:length(S), string:reverse(S)}
+        end)
+    ]}.
 
 %--- Internal ------------------------------------------------------------------
 
